039bde04f964c10249f4df485fa6bba2dc7b69f7
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2019-04-08 11:06:23 +0800

    Fixed minimizing

--- basebox/minimize.sh
+++ basebox/minimize.sh
@@ -1,7 +1,8 @@
-#!/bin/sh -eux
+#!/bin/sh
 
-dd if=/dev/zero of=/EMPTY bs=1M
+dd if=/dev/zero of=/EMPTY bs=100M
 rm -f /EMPTY
 # Block until the empty file has been removed, otherwise, Packer
 # will try to kill the box while the disk is still full and that's bad
 sync
+echo "Done minimizing"
